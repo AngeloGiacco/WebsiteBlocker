c4ab9219838e4cd13f80c9fed7477910fee3aa7b
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-09-20 09:26:51 +0100

    Blocks defined websites in certain times
    This python code designed for mac blocks defined websites during certain hours. It uses the hosts file on the computer to redirect the user to the localhost.

---
+++ websiteblocker.py
@@ -0,0 +1,26 @@
+import time
+from datetime import datetime as dt
+redirect = "127.0.0.1"
+hosts_path = "/private/etc/hosts"
+website_list=["www.facebook.com","facebook.com","youtube.com","www.youtube.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 22):
+        print("Working hours")
+        with open(hosts_path, "r+") as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website + "\n")
+    else:
+        with open(hosts_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("remove censor")
+    time.sleep(60)
